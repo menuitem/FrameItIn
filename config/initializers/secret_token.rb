08020b68a7d170af44b6fea2b5f1d9b22d8fc838
Lucjan Stepien <lucjan.stepien@student.ncirl.ie>
2014-04-07 17:28:48 +0100

    Initial commit, bare rails project created.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FrameItIn::Application.config.secret_token = '86c952c6638c5ca241eaefda755c6a8fee6a0a5d71e9a37d3520a6f80d3ae09003b580bc73ebded7a1dd356819e5e57f1fafb64143e8c30bf281bf3a56032fe4'
