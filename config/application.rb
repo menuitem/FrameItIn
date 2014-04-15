9dfc2c59255bfd3107a7c466aab931df9bfbb2f2
menuitem <mishmash111@gmail.com>
2014-04-15 16:31:27 +0100

    - added users (devise) - need to run: rake de:migrate

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,10 @@ module FrameItIn
   class Application < Rails::Application
     # VCS
     config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
+
+    #devise setting 
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
